cf5ad30cee70ba13d22316ff943c242ad3a74f3b
Raymond Forbes <rforbes@mozilla.com>
2018-04-02 17:09:16 -0700

    Update testscript.ps1

--- testscript.ps1
+++ testscript.ps1
@@ -10,6 +10,8 @@ $password =  ConvertTo-SecureString $vmAdminPassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:USERDOMAIN\$vmAdminUsername", $password)
  
 Write-Verbose -Verbose "Entering Custom Script Extension..."
+
+Enable-PSRemoting -force
  
 Invoke-Command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $PSScriptRoot -ScriptBlock {
   param 
